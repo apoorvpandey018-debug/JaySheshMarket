7b2d97359bfec5fbbde029ea45d5d062896e2a00
apoorvpandey018-debug <apoorvpandey018@gmail.com>
2026-02-19 13:54:21 +0530

    Add files via upload

---
+++ Apoorv.js
@@ -0,0 +1,3 @@
+alert("Welcome to Jay Shesh Market Project!");
+var a = prompt("Please enter your name to continue:");
+alert("Hello " + a + "! Welcome to the Jay Shesh Market Project! We hope you have a great experience exploring our project.");
